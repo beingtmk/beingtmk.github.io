a1bb1490f92298b2f213e966ea1e08fb27e34d15
Mithilesh Kohale <beingtmk@gmail.com>
2019-08-25 15:25:05 +0530

    modified for myself

---
+++ build/precache-manifest.d913faf77008b7cfcad2615db43f2b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6f6ee3daf3aecd790ad",
+    "url": "/portfolio-dev/static/js/runtime~main.a81e6e83.js"
+  },
+  {
+    "revision": "32c2a8763187491e991f",
+    "url": "/portfolio-dev/static/js/main.4b504258.chunk.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/portfolio-dev/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "32c2a8763187491e991f",
+    "url": "/portfolio-dev/static/css/main.5dba743b.chunk.css"
+  },
+  {
+    "revision": "482ff3df4b5b9e9ce90a2a8549161054",
+    "url": "/portfolio-dev/index.html"
+  }
+];
