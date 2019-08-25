0eda6416afaf474e464860ff324f4178996bb9c3
Mithilesh Kohale <beingtmk@gmail.com>
2019-08-25 15:26:41 +0530

    build

--- build/precache-manifest.a9245b28533c828b999faf150e25a51b.js
+++ build/precache-manifest.a9245b28533c828b999faf150e25a51b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio-dev/static/js/runtime~main.a81e6e83.js"
   },
   {
-    "revision": "32c2a8763187491e991f",
-    "url": "/portfolio-dev/static/js/main.4b504258.chunk.js"
+    "revision": "30e3c781614fb2c51d35",
+    "url": "/portfolio-dev/static/js/main.79aea140.chunk.js"
   },
   {
     "revision": "c7652bb8ab0a13c4364d",
     "url": "/portfolio-dev/static/js/2.6cb2980a.chunk.js"
   },
   {
-    "revision": "32c2a8763187491e991f",
+    "revision": "30e3c781614fb2c51d35",
     "url": "/portfolio-dev/static/css/main.5dba743b.chunk.css"
   },
   {
-    "revision": "482ff3df4b5b9e9ce90a2a8549161054",
+    "revision": "c3a8e3ba77f2f83437437597555004d0",
     "url": "/portfolio-dev/index.html"
   }
 ];
