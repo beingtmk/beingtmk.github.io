ad7492344fd73ef778e5b1846458514e860c8be3
lokeshrana9999 <lokeshrana9999@gmail.com>
2019-08-27 01:15:35 +0530

    fixed deployment

---
+++ build/precache-manifest.fda6ab4f517398aabc18a4667e5a1185.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d4e8d41c8e3285b6670",
+    "url": "/static/js/main.7fdae88f.chunk.js"
+  },
+  {
+    "revision": "9c1d8f9c30f0865a2581",
+    "url": "/static/js/2.45e8f200.chunk.js"
+  },
+  {
+    "revision": "7d4e8d41c8e3285b6670",
+    "url": "/static/css/main.d152eb43.chunk.css"
+  },
+  {
+    "revision": "0b88a725d4aff2deac65f2a554d6103c",
+    "url": "/index.html"
+  }
+];
